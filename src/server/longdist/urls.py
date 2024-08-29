ef5c57b0c4253094030a32f2c966140db0f6b4f5
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2024-08-29 10:17:25 -0700

    moved serializers and viewsets to views.py

--- src/server/longdist/urls.py
+++ src/server/longdist/urls.py
@@ -16,64 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.models import User
-from longdist.models import Pin, Message, Relationship
-from rest_framework import routers, serializers, viewsets
-
-# Serializers define the API representation.
-'''
-useful for filtering on a queryset
-
-User.objects.all() <-- user is django object
-making a SQL query and returning a "queryset" object
-
-defining a subset of fields that it's returning
-
-don't need the rest for my view
-
-django rest framework thing haha
-
-return it as json
-
-without the serializer you would have to loop through the queryset itself
-
-print out what the object looks like before and after you run it through
-the serializer
-
-you can use it for validation of fields (inbound)
-
-some inbound and some outbound
-
-builtin validate function (look for this)
-
-CORS for validating frontend calls to my api
-
-django app is going to have http headers that it sets and checks that interact
-with headers set and checked by the client
-
-what domain it's on
-
-you can restrict it this way
-
-https://pypi.org/project/django-cors-headers/
-
-might want to move serializers to their own file
-
-look at django docs
-'''
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+from longdist.views import router
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
---
+++ src/server/longdist/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from longdist.models import Pin, Message, Relationship
+from rest_framework import routers, serializers, viewsets
+from datetime import datetime
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
