d0bb84e0b01172284fb683e6e5c6d1afdc7017cc
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2024-11-12 12:49:43 -0800

    Fix load guard pages

--- src/client/src/components/SorryNoMobile.tsx
+++ src/client/src/components/SorryNoMobile.tsx
@@ -1,9 +1,9 @@
 function SorryNoMobile() {
     return(
         <>        
-            <p>Sorry, you have to use this device on a larger screen!</p>
+            <p>Sorry, you have to use this site on a larger screen!</p>
             <br/>
-            <p>See what the site is usually like here.</p>
+            <p>See what the site is usually like <a href="https://github.com/shenaichan/long_distance">here</a>.</p>
         </>
     )
 }
--- src/client/src/components/SorryTooMuchApi.tsx
+++ src/client/src/components/SorryTooMuchApi.tsx
@@ -3,9 +3,9 @@ function SorryTooMuchApi() {
         <>        
             <p>Sorry, the site is locked until the first of next month due to the API being too expensive :(</p>
             <br/>
-            <p>Donate to the site's continued existence here!</p>
+            <p>Donate to the site's continued existence here (link coming soon)!</p>
             <br/>
-            <p>See what the site is usually like here.</p>
+            <p>See what the site is usually like <a href="https://github.com/shenaichan/long_distance">here</a>.</p>
         </>
     )
 }
